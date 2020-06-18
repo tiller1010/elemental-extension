920716323eeba8fca85412cb42880815b0dbc45f
Tyler Trout <tyler@werkbot.com>
2020-06-17 21:54:47 -0400

    React transform

--- client/dist/js/main.bundle.js
+++ client/dist/js/main.bundle.js
@@ -69,9 +69,50 @@
 /******/ ({
 
 /***/ "./client/src/js/src/main.js":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_lib_Injector__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_lib_Injector___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_lib_Injector__);
+
+
+
+var CharacterCounter = function CharacterCounter(TextField) {
+	return function (props) {
+		return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+			'div',
+			null,
+			__WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(TextField, props),
+			__WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+				'small',
+				null,
+				'Character count: ',
+				props.value.length
+			)
+		);
+	};
+};
+
+__WEBPACK_IMPORTED_MODULE_1_lib_Injector___default.a.transform('character-count-transform', function (updater) {
+	updater.component('TextField', CharacterCounter);
+});
+
+/***/ }),
+
+/***/ 0:
 /***/ (function(module, exports) {
 
-console.log('hello');
+module.exports = Injector;
+
+/***/ }),
+
+/***/ 1:
+/***/ (function(module, exports) {
+
+module.exports = React;
 
 /***/ })
 
--- client/src/js/src/main.js
+++ client/src/js/src/main.js
@@ -1,1 +1,16 @@
-console.log('hello')
+import React from 'react';
+import Injector from 'lib/Injector';
+
+const CharacterCounter = (TextField) => (props) => {
+	return (
+		<div>
+			<TextField {...props} />
+			<small>Character count: {props.value.length}</small>
+		</div>
+	);
+}
+
+
+Injector.transform('character-count-transform', (updater) => {
+  updater.component('TextField', CharacterCounter);
+});
